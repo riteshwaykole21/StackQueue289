41a5a0ae6371a34a65502ef8d1dd5035895943bc
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-07-18 09:16:12 +0530

    [Ritesh]add : Stack Linked List

---
+++ StackQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack and Queue Linked List");
+            StackLinkedList stackLinkedList = new StackLinkedList();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
